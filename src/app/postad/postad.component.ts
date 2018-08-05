d23f2e16cde77ed73cc773332f4d06d1ffa0b181
rdtharindudk <dilshantharinduk@gmail.com>
2018-08-05 11:32:22 +0530

    implementing new advert

--- src/app/postad/postad.component.ts
+++ src/app/postad/postad.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FileUploadModule, FileUploader } from 'ng2-file-upload';
 
 @Component({
   selector: 'app-postad',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostadComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public uploader: FileUploader = new FileUploader({ url: 'http://localhost:3000/api/advert/new', itemAlias: 'photos' });
 
   ngOnInit() {
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+    this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+      console.log('ImageUpload:uploaded:', item, status, response);
+      alert('File uploaded successfully');
+    };
   }
+
+  onSubmit(details) {
+    console.log(details);
+    this.uploader.onBuildItemForm = (item, form) => {
+      form.append('title', 'tharindu');
+    };
+    this.uploader.uploadAll();
+  }
+
+
 
 }
